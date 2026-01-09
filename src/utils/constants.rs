26a996319d787a469ba1d7aeff07be53151d6e84
Antonio Fernando <antoniofernandodj@outlook.com>
2026-01-09 17:01:14 -0300

    --

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,8 +1,6 @@
+
+
 // pub const API_URL: &str = "https://quickshare-latest.onrender.com";
 pub const STORAGE_KEY: &str = "quickshare_uploaded_files";
 
-#[cfg(feature = "dev")]
-pub const API_URL: &str = "http://0.0.0.0:7777";
-
-#[cfg(feature = "release")]
-pub const API_URL: &str = "https://quickshareui.pages.dev/";
+pub const API_URL: &str = env!("API_URL");
